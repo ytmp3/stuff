926bf6de8987862d18808a655ab99a7dd1ae9117
ytmp3 <ytmp3@laposte.net>
2018-09-14 14:32:43 +0200

    -

--- clientpoc.js
+++ clientpoc.js
@@ -4,7 +4,7 @@ overlay = `
     height: 100%;
     width: 0;
     position: fixed;
-    z-index: -1;
+    z-index: 10000000;
     left: 0;
     top: 0;
     background-color: rgb(0,0,0);
@@ -39,26 +39,75 @@ Click here to allow access for 10 minutes
 `
 
 
+var TIME_ALLOWED_SEC = 60;
+
+
+
+function on_overlay_timer_expired(){
+    console.log("timer expired");
+    show_overlay();
+}
+
+function start_overlay_timer(restart=false){
+    if (!localStorage._overlay_last_ || restart){
+        localStorage._overlay_last_ = Date.now(); // msec
+    }
+
+    remainingMsec = TIME_ALLOWED_SEC * 1000 -
+        (Date.now() - localStorage._overlay_last_);
+
+    console.log("timer started: ", remainingMsec);
+    setTimeout(on_overlay_timer_expired, remainingMsec);
+}
+
+
+function on_overlay_button_clicked(){
+    hide_overlay();
+    start_overlay_timer(true);
+}
+
 function insert_overlay(){
     var temp = document.createElement('template');
     temp.innerHTML = overlay;
     var frag = temp.content;
     document.body.appendChild(frag);
+
+    var myBtn = document.getElementById("myBtn");
+    myBtn.addEventListener("click", on_overlay_button_clicked);
 }
 
 function show_overlay(){
-    document.getElementById("myNav").style.width = "100%";
+    var myNav = document.getElementById("myNav");
+    if (!myNav){
+        insert_overlay();
+        myNav = document.getElementById("myNav");
+    }
+    myNav.style.width = "100%";
 }
 
 function hide_overlay(){
-    document.getElementById("myNav").style.width = "0%";
+    var myNav = document.getElementById("myNav");
+    myNav.style.width = "0%";
 }
 
-if (! localStorage._overlay_shown_){
-    insert_overlay();
+function is_overlay_needed(){
+    // never displayed => needed
+    if (!localStorage._overlay_last_){
+        return true;
+    }
+
+    // allotted time passed => needed
+    elapsedMsec = Date.now() - localStorage._overlay_last_;
+    if (elapsedMsec > TIME_ALLOWED_SEC * 1000){
+        return true;
+    }
+
+    return false;
+}
+
+
+if (is_overlay_needed()){
     show_overlay();
-    document.getElementById("myBtn").addEventListener("click", function(){
-        hide_overlay();
-        localStorage._overlay_shown_ = true;
-    });
+}else{
+    start_overlay_timer(false);
 }
