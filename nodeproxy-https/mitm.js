f94e0c0fd9c18d86622b90095fc5a1c20e5c1050
ytmp3 <ytmp3@laposte.net>
2018-10-17 14:46:06 +0200

    code cleanup of 'stub' approach

--- nodeproxy-https/mitm.js
+++ nodeproxy-https/mitm.js
@@ -1,18 +1,52 @@
+"use strict";
 
-var Proxy = require('http-mitm-proxy');
+const fs      = require('fs');
+const net     = require('net');
+const url     = require('url');
+const http    = require('http');
+const process = require('process');
+const Proxy   = require('http-mitm-proxy');
+const Base64encode = require('base64-stream').encode;
 
-var fs = require('fs');
-var net = require('net');
-var url = require('url');
-var http = require('http');
-var process = require('process');
-var Base64encode = require('base64-stream').encode;
 
 /* cdn to load the 'pako' compression library */
-const PAKO_CDN_URL =
-        'https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.6/pako_inflate.min.js';
+const PAKO_CDN_URL = 'https://cdnjs.cloudflare.com'+
+      '/ajax/libs/pako/1.0.6/pako_inflate.min.js';
 
-const DISABLE_COMPRESSION = true;
+const ENABLE_INJECTION = true;
+const ENABLE_COMPRESSION = false;
+const PROXY_PORT = 8081;
+
+const proxy = Proxy();
+
+
+
+/*
+note:
+ - clientToProxyRequest: IncomingMessage
+ - proxyToClientResponse: ServerResponse
+ - proxyToServerRequest: ClientRequest
+ - serverToProxyResponse: IncomingMessage
+*/
+
+
+function override(object, methodName, callback)
+{
+    object[methodName] = callback(object[methodName]);
+}
+
+
+// Monkey patch node-http-mitm-proxy.Proxy._onHttpServerConnect()
+// to avoid ECONNRESET unhandled exception
+override(proxy, '_onHttpServerConnect', function(original) {
+    return function(req, socket, head) {
+        socket.addListener("error", (err) => {
+            console.log("**** _onHttpServerConnect: socket error", err);
+        });
+        return original.apply(this, arguments);
+    };
+});
+
 
 /**
  * build injected template
@@ -41,126 +75,132 @@ function build_template_post(args){
     return post_data;
 }
 
-function override(object, methodName, callback)
+function onError(ctx, err, errorKind)
 {
-  object[methodName] = callback(object[methodName])
+    const url = (ctx && ctx.clientToProxyRequest) ?
+        ctx.clientToProxyRequest.url : "n/a";
+    console.error("**** " + errorKind + ' on ' + url + ':', err);
 }
 
-var proxy = Proxy();
-
-// Monkey patch node-http-mitm-proxy.Proxy._onHttpServerConnect()
-// to avoid ECONNRESET unhandled exception
-override(proxy, '_onHttpServerConnect', function(original) {
-  return function(req, socket, head) {
-    socket.addListener("error", (err) =>
-    {
-      console.log("onProxyReceiveConnect: socket error");
-    });
-    return original.apply(this, arguments)
-  }
-});
-
-proxy.onError(function(ctx, err)
+/**
+ * handle request header from server
+ *
+ */
+function onRequest(ctx, callback)
 {
-    console.error('proxy error:', err);
-});
-
-let enableInjection = true;
-
-proxy.onRequest(function(ctx, callback)
-{
-  ctx.b64encoder = new Base64encode();
-  ctx.b64encoder.pipe(ctx.proxyToClientResponse);
-
-  ctx.doInjection = false;
-  ctx.injectionStarted = false;
-
-  ctx.proxyToServerRequestOptions.headers['accept-encoding'] = 'identity';
-
-  // console.log(ctx.proxyToServerRequestOptions.headers);
-  // console.log("==================================================");
-
-  return callback();
-});
-
-proxy.onResponse(function(ctx, callback)
-{
-
-  delete ctx.serverToProxyResponse.headers['content-security-policy'];
-  delete ctx.serverToProxyResponse.headers['expect-ct'];
-  if (ctx.serverToProxyResponse.headers['content-type'] &&
-      ctx.serverToProxyResponse.headers['content-type'].startsWith('text/html') &&
-      ! ('x-requested-with' in ctx.clientToProxyRequest.headers))
-  {
-    ctx.doInjection = enableInjection;
-  }
-  else
-  {
+    // true if javascript injection must be performed on this request
+    // (current criteria is: content-type is text/html
     ctx.doInjection = false;
-  }
-  return callback();
-});
+    ctx.injectionStarted = false;
 
-proxy.onResponseData(function(ctx, chunk, callback)
+    if (!ENABLE_COMPRESSION){
+        const svr_req_headers = ctx.proxyToServerRequestOptions.headers;
+        svr_req_headers['accept-encoding'] = 'identity';
+    }
+    return callback();
+}
+
+
+/**
+ * handle response header from server
+ *
+ */
+function onResponse(ctx, callback)
 {
-  if (ctx.doInjection) // Perform injection
-  {
-    if (!ctx.injectionStarted)
+
+    const resp_headers = ctx.serverToProxyResponse.headers;
+    const req_headers = ctx.clientToProxyRequest.headers;
+
+    // !!!! todo: for now we remove the csp header
+    delete resp_headers['content-security-policy'];
+
+    /* The Expect-CT header allows sites to opt in to reporting
+     * and/or enforcement of Certificate Transparency
+     * requirements, which prevents the use of misissued
+     * certificates for that site from going unnoticed.
+     */
+    delete resp_headers['expect-ct'];
+
+    if (resp_headers['content-type'] &&
+        resp_headers['content-type'].startsWith('text/html') &&
+        ! ('x-requested-with' in req_headers))
     {
-      //console.log(ctx.serverToProxyResponse.headers);
-      let resp_encoding = ctx.serverToProxyResponse.headers["content-encoding"];
+        ctx.doInjection = ENABLE_INJECTION;
+        delete resp_headers['content-length'];
+    }
+    return callback();
+}
 
-      let compressed = (resp_encoding == "gzip");
-      let pre_data = build_template_pre({ compressed });
-      if (compressed)
-      {
-        ctx.use(Proxy.gunzip);
-      }
 
-      ctx.proxyToClientResponse.write(pre_data);
-      ctx.injectionStarted = true;
+/**
+ * handle response data from server
+ *
+ */
+function onResponseData(ctx, chunk, callback)
+{
+    if (!ctx.doInjection){
+        return callback(null, chunk);
+    }
+
+    if (!ctx.injectionStarted){
+        const resp_headers = ctx.serverToProxyResponse.headers;
+        const resp_encoding = resp_headers["content-encoding"];
+        const compressed = (resp_encoding == "gzip");
+
+        if (compressed)
+        {
+            ctx.use(Proxy.gunzip);
+        }
+
+        const pre_data = build_template_pre({compressed});
+        ctx.proxyToClientResponse.write(pre_data);
+        ctx.injectionStarted = true;
+
+        ctx.b64encoder = new Base64encode();
+        ctx.b64encoder.pipe(ctx.proxyToClientResponse);
     }
     ctx.b64encoder.write(chunk);
     return callback(null, null);
-  }
-  else // or not
-  {
-    return callback(null, chunk);
-  }
-});
+}
 
-  //console.log(ctx.clientToProxyRequest.headers);
-  //console.log(ctx.clientToProxyRequest.headers['host'] + ctx.clientToProxyRequest.url);
-
-proxy.onResponseEnd(function(ctx, callback)
+/**
+ * handle response end from server
+ *
+ */
+function onResponseEnd(ctx, callback)
 {
-  // console.log(ctx.serverToProxyResponse.headers);
+    const resp_headers = ctx.serverToProxyResponse.headers;
+    if (resp_headers['transfer-encoding'] != 'chunked'){
+        console.log('onResponseEnd:!!!!!!!!!!!!!!!!');
+        throw 0;
+    }
 
-  if (ctx.serverToProxyResponse.headers['transfer-encoding'] != 'chunked')
-  {
-    console.log(ctx.serverToProxyResponse);
-    console.log(ctx.clientToProxyRequest);
-    throw 0;
-  }
+    if (ctx.doInjection)
+    {
+        const inline_js = fs.readFileSync("injected_page.js", "utf8");;
+        const post_data = build_template_post({inline_js});
+        ctx.proxyToClientResponse.write(post_data);
+    }
 
-  if (ctx.doInjection)
-  {
-    let inline_js = fs.readFileSync("injected_page.js", "utf8");;
-    let post_data = build_template_post({inline_js});
+    return callback();
+}
 
-    ctx.proxyToClientResponse.write(post_data);
-  }
 
-  return callback();
+proxy.onError(onError);
+proxy.onRequest(onRequest);
+proxy.onResponse(onResponse);
+proxy.onResponseData(onResponseData);
+proxy.onResponseEnd(onResponseEnd);
+
+
+
+proxy.listen({
+    port: PROXY_PORT,
+    silent:false
 });
 
-proxy.listen({ port: 8081 });
-
+const ca_path = proxy.sslCaDir + '/certs/ca.pem';
+// const ca = fs.readFileSync(ca_path, {encoding: "ascii"});
 console.log("Make sure your browser trusts this CA:");
-require('fs').readFile(
-  proxy.sslCaDir + '/certs/ca.pem',
-  function(err, buf)
-  {
-    process.stdout.write(buf);
-  });
-
+console.log(ca_path);
+console.log("Proxy running on port", PROXY_PORT);
